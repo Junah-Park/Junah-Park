9ed17f3a9c986ead4d4dd97d17bda9f158562f1b
Junah Park <junahp@hotmail.com>
2020-10-20 16:31:58 -0700

    alc image

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -11,22 +11,21 @@ class Footer extends Component {
 
     return (
       <footer>
+        <div className="row">
+            <div className="twelve columns">
+              <ul className="social-links">
+                  {networks}
+              </ul>
 
-     <div className="row">
-        <div className="twelve columns">
-           <ul className="social-links">
-              {networks}
-           </ul>
+              <ul className="copyright">
+                  <li>&copy; Copyright 2020 Junah Park</li>
+                  {/* <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li> */}
+              </ul>
 
-           <ul className="copyright">
-              <li>&copy; Copyright 2020 Junah Park</li>
-              {/* <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li> */}
-           </ul>
-
+            </div>
+            <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#about"><i className="icon-up-open"></i></a></div>
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#about"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
+      </footer>
     );
   }
 }
